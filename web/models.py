fc6e87049f883f2782c8176d12c51db476c20ab1
yayang511 <11035124@dcsh.tp.edu.tw>
2022-12-16 11:17:44 +0800

    Message Model 2

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    user=models.CharField('姓名', max_length=16)
+    subject=models.CharField('主旨',max_length=200)
+    content=models.TextField('內容')
+    created=models.DateTimeField('留言時間',auto_now_add=True)
+
+    def __str__(self):
+        return self.user + ': ' + self.subject
